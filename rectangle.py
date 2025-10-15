ed537cee548c4e496198484e689279d1f3dc3ad4
affishh <afrinshaik802@gmail.com>
2025-10-15 13:39:42 +0000

    added codes

--- rectangle.py
+++ rectangle.py
@@ -8,10 +8,22 @@ class Rectangle:
     def perimeter(self):
         return 2 * (self.length + self.width)
 
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height        
+    
+
+
 def main():
     rect = Rectangle(5, 8)
+    tri = Triangle(5, 8)
     print("Area:", rect.area())
     print("Perimeter:", rect.perimeter())
+    print("Area:", tri.area())
 
 if __name__ == "__main__":
     main()
--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from rectangle import Rectangle
+from rectangle import Rectangle, Triangle
 
 def test_area():
     rect = Rectangle(5, 8)
@@ -8,3 +8,7 @@ def test_area():
 def test_perimeter():
     rect = Rectangle(5, 8)
     assert rect.perimeter() == 26
+
+def test_triangle_area():
+    tri = Triangle(5, 8)
+    assert tri.area() == 20
