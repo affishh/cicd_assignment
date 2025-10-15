83f50f9eed76e45c31f4062d1e0c1d398e5829c1
affishh <afrinshaik802@gmail.com>
2025-10-15 17:57:21 +0000

    Add package.json for Jenkins build

--- jenkins/app.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello from Jenkins app.js");
-console.log("This is a simple Node.js application.");
-console.log("It is used to demonstrate Jenkins CI/CD pipeline.");
-console.log("This application has been updated.");
-console.log("New features have been added.");
-console.log("End of the application.");
