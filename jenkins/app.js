441faa7759d95861a933d51a7ea1a8564b62ecd3
affishh <afrinshaik802@gmail.com>
2025-10-15 16:22:15 +0000

    added codes

--- jenkins/app.js
+++ jenkins/app.js
@@ -1,2 +1,4 @@
 console.log("Hello from Jenkins app.js");
 console.log("This is a simple Node.js application.");
+console.log("It is used to demonstrate Jenkins CI/CD pipeline.");
+console.log("This application has been updated.");
